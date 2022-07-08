898b63521e27fbd5a12bbf73c378d481ef8a57ab
patrick <psharp@greenspice.com>
2022-07-07 18:17:37 -0700

    double whoops

--- initial-xv6/src/syscall.c
+++ initial-xv6/src/syscall.c
@@ -103,7 +103,7 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
-extern int sys_getreadcounts(void);
+extern int sys_getreadcount(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -127,7 +127,7 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
-[SYS_getreadcount]   sys_getreadcounts,
+[SYS_getreadcount]   sys_getreadcount,
 };
 
 void
