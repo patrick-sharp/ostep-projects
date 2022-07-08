5cc354c40419782b7a21c5ae14d6a3a7ad3399ad
patrick <psharp@greenspice.com>
2022-07-07 18:03:07 -0700

    whoops, wrong varname

--- initial-xv6/src/syscall.c
+++ initial-xv6/src/syscall.c
@@ -127,7 +127,7 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
-[SYS_getreadcounts]   sys_getreadcounts,
+[SYS_getreadcount]   sys_getreadcounts,
 };
 
 void
