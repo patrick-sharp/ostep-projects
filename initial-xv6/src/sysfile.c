c369977441a839b0d8d6b198f57da35322c79df2
patrick <psharp@greenspice.com>
2022-07-11 21:23:00 -0700

    Pass both tests for initial-xv6

--- initial-xv6/src/sysfile.c
+++ initial-xv6/src/sysfile.c
@@ -75,9 +75,9 @@ sys_read(void)
   int n;
   char *p;
 
+  readcount++;
   if(argfd(0, 0, &f) < 0 || argint(2, &n) < 0 || argptr(1, &p, n) < 0)
     return -1;
-  readcount++;
   return fileread(f, p, n);
 }
 
